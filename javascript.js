3cac8f654b88a3a41c07d427918a603cc81bab12
Ningaro <baev.dima.77@gmail.com>
2018-07-17 11:48:08 +0300

    Recoding to userscripts

--- javascript.js
+++ javascript.js
@@ -1,28 +1,45 @@
-//InstPause second version by Ningaro
+console.log('InstPause ready for work :)');
+var nIntervId;
 var InstTurn = 0;
-var targetNode = document.querySelector("div[class*='B20bj']");
+var nextErrorWillBeFirst = 1;
 
-function triggerMouseEvent(node, eventType) {
-  var clickEvent = document.createEvent('MouseEvents');
-  clickEvent.initEvent(eventType, true, true);
-  node.dispatchEvent(clickEvent);
-  if (InstTurn == 1)
-    setTimeout("triggerMouseEvent(targetNode, 'mousedown')", 1);
+function triggerMouseEvent() {
+  var event = document.createEvent('MouseEvents');
+  event.initEvent('mousedown', true, true);
+  var targetNode = document.querySelector("div[class*='B20bj']");
+  if (targetNode) {
+      targetNode.dispatchEvent(event);
+      nextErrorWillBeFirst = 1;
+  } else {
+      if (nextErrorWillBeFirst == 1) {
+          // для повторяющихся событий выводим в лог только один раз
+          console.log("[InstPause] Story window not found!");
+          // и выключаем обработку
+          switchOff();
+      }
+      nextErrorWillBeFirst = 0;
+  }
 }
+
+function switchOn(){
+   console.log('[InstPause] Turn on!');
+   InstTurn = 1;
+   nextErrorWillBeFirst = 1;
+   nIntervId = setInterval(triggerMouseEvent, 1);
+}
+
+function switchOff(){
+   console.log('[InstPause] Turn off!');
+   InstTurn = 0;
+   clearInterval(nIntervId);
+}
+
 document.addEventListener('keydown', (event) => {
   if (event.keyCode == 80) {
     if (InstTurn == 0) {
-      console.log('[InstPause] Turn on!');
-      InstTurn = 1;
-      targetNode = document.querySelector("div[class*='B20bj']");
-      triggerMouseEvent(targetNode, 'mousedown');
+      switchOn();
     } else {
-      console.log('[InstPause] Turn off!');
-      InstTurn = 0;
+      switchOff();
     }
   }
 });
-if (targetNode) {
-  triggerMouseEvent(targetNode, 'mousedown');
-} else
-  console.log("[InstPause] Story window not found!");
